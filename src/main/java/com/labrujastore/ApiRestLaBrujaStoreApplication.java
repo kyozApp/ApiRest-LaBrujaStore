61b7d0b2d163b20647b8e9c12e0271d47f3470fa
kyozApp <151455520+kyozApp@users.noreply.github.com>
2024-03-03 18:58:43 -0500

    first commit

---
+++ src/main/java/com/labrujastore/ApiRestLaBrujaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.labrujastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestLaBrujaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestLaBrujaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labrujastore/ApiRestLaBrujaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labrujastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestLaBrujaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
